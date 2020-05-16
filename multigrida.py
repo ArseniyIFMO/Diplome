22ba9183174e51add95f5d7a3a77d70f1317b94b
Arseniy <vatrushka.96@mail.ru>
2020-05-16 13:50:13 +0300

    Update multigrida.py

--- multigrida.py
+++ multigrida.py
@@ -1,10 +1,27 @@
 import numpy as np
 def norm(R):
-    return np.amax(R)
+    max = np.amax(R)
+    min = np.amin(R)
+    min = min * (-1)
+    if(max > min):
+        return max
+    else:
+        return min
+
+def reshape(x):
+    sz = x.size
+    print(sz)
+    f = np.zeros(2 * sz - 1)
+    for i in range(0, sz):
+        f[2 * i] = x[i]
+    print(f)
+    for i in range(0, sz - 1):
+        f[2 * i + 1] = (f[2 * i] + f[2 * i + 2]) / 2
+    return f
 
 N = 5
 eps = 0.001
-n = 5
+n = 9
 alpha = 0.01
 f = np.zeros(n)
 b = np.random.randn(n)
@@ -24,10 +41,13 @@ for i in range(1,  n-1):
     A[i][i + 1] = 1
 for i in range(0, n):
     b[i] = 1
-print(A)
-while(norm(f) > eps):
-    RF = np.copy(b)
-    print(A.dot(f))
-    Af = A.dot(f)
-    RF = RF - Af
-    f = f - RF * alpha
+print(b)
+print(reshape(b))
+
+#while(norm(f) > eps):
+#    RF = np.copy(b)
+#    print(f)
+#    Af = A.dot(f)
+#    RF = RF - Af
+#    f = f - RF * alpha
+
